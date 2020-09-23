2f235d048ef70ad776c95c02470118fa241aefba
gr523 <tasnim0990@gmail.com>
2020-09-23 13:39:13 +0600

    Wed Sep23 2020

---
+++ TakeToclass/routine.js
@@ -0,0 +1,34 @@
+function Monday(){
+    today.addclass("Math 4241 - MA ",9,0,10,50,
+    "https://bdren.zoom.us/j/4160336678",
+    "416 033 6678","No password needed");
+
+    today.addclass("HUM 4242 - AHM",16,15,18,5);
+    today.addclass("CSE 4205 - MHA",14,10,15,0);
+    today.addclass("CSE 4203 - MRK",11,0,11,50);
+}
+
+function Tuesday(){
+    today.addclass("CSE 4205 - MHA",9,0,9,50);
+    today.addclass("HUM 4241 - MSH",12,0,12,50);
+    
+    today.addclass("CSE 4203 - MRK",14,10,15,0,
+    "https://meet.google.com/lookup/fo7t6dy74k");
+
+    today.addclass("CSE 4202(1A) - FS,TIA",15,10,18,5);
+}
+
+function Wednesday(){
+    today.addclass("Phy 4241 - FAK",9,0,10,15,
+    "https://bdren.zoom.us/j/69963926248?pwd=VlVjMHBlQlJSVUFob0N4aHlwRHRUQT09","699 6392 6248","897645");
+    today.addclass("Math 4241 - MA",11,0,12,50,
+        "https://bdren.zoom.us/j/4160336678");
+    today.addclass("ACM Programming",15,10,18,5);
+}
+
+function Thursday(){
+    today.addclass("HUM 4241 - MSH",9,0,9,50);
+    today.addclass("CSE 4205 - MHA",10,0,10,50);
+    today.addclass("Chem 4241 - NI",11,35,12,50);
+    today.addclass("CSE 4203 - MRK",14,10,15,0);
+}
