687c279ccb228f807fdc76c6317abd6bdb439c80
gr523 <tasnim0990@gmail.com>
2021-01-16 13:57:38 +0600

    Sat Jan16 2021

--- Testing/justout.cpp
+++ Testing/justout.cpp
@@ -2,6 +2,13 @@
 #define ll long long 
 using namespace std;
 int main(){
-    cout<<s<<"\n";
-    int x=12; 
+    int n=12,c=1,i=1;
+    while(c<=n){
+        for(int j=1;j<=i;j++){
+            cout<<c<<" ";
+            if(c++>=n) break;
+        }
+        i++;
+        cout<<"\n";
+    }
 }
